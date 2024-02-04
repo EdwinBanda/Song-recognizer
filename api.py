12cf8bd844390dd700b9bbc6b89bb3e8afb4bedc
EdwinBanda <casemirobanda@gmail.com>
2024-02-04 22:13:45 +0200

    refactor: ...created a .env file to store the sensetive information and get it to the code

--- api.py
+++ api.py
@@ -4,7 +4,7 @@ import base64
 import requests
 
 def gravarAudio(nomeArquivo, duracaoSegundos):
-    print("Recording...")
+    print("Gravando...")
     audio = sd.rec(int(duracaoSegundos * 44100), samplerate=44100, channels=1, dtype='int16')
     sd.wait()
     # print("Gravação concluída!")
@@ -12,9 +12,10 @@ def gravarAudio(nomeArquivo, duracaoSegundos):
     # return audio
 
 def analisarAudio(audio, apiKey):
-    print("Analizyng audio...")
+    print("Analizando o audio...")
     audioData = open(nomeArquivo, 'rb').read()
     audioDataBase64 = base64.b64encode(audioData).decode('utf-8')
+    # print(audioDataBase64)
 
 
     url = "https://shazam.p.rapidapi.com/songs/detect"
@@ -26,10 +27,11 @@ def analisarAudio(audio, apiKey):
     headers = {
         "content-type": "text/plain",
         "x-rapidapi-host": "shazam.p.rapidapi.com",
-        "x-rapidapi-key": "ENTER_YOUR_API_KEY"
+        "x-rapidapi-key": "e3b5e998ffmsh19c54b23a5a6031p1f1fa1jsn0e35a3fc6c22"
     }
 
     data = audioDataBase64
+    print(data)
 
     try:
         response = requests.request("POST",url, data=data, headers=headers)
@@ -40,12 +42,12 @@ def analisarAudio(audio, apiKey):
             song['track']['title'],song['track']['subtitle']
         ]
     except requests.exceptions.RequestException as e:
-        print("Error making API request:", e)
+        print("Erro fazendo request na API:", e)
         return None
 
 nomeArquivo = "gravacao.wav"
 duracaoSegundos = 5
-apiKey = "ENTER_YOUR_API_KEY"
+apiKey = process.env.apiKey
 
 audio = gravarAudio(nomeArquivo, duracaoSegundos)
 
@@ -54,4 +56,4 @@ resultadoAnalise = analisarAudio(audio, apiKey)
 if resultadoAnalise is not None:
     print(resultadoAnalise)
 else:
-    print("Could not get a response from the API.")
+    print("Problemas com a API")
